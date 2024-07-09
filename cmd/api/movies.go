5fcda99f35d4a0aa265bb9952a2536bd0d7ae0d1
hyprhex <swapapis@gmail.com>
2024-07-09 19:27:02 +0100

    Write helper function to get id without need to reapte it every time we need id

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,16 +10,11 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// Return a slice (key and value) of url params
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// Get and convert the ID
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	// Print out ID
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
