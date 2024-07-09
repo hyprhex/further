01361b093770cdcc8e697b4c6621639cc934c28f
hyprhex <swapapis@gmail.com>
2024-07-09 19:09:50 +0100

    Encapsulating API routes and using httprouter instead of servemux

---
+++ cmd/api/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	// Initialize a httprouter router
+	router := httprouter.New()
+
+	// Register URL pattern and handlers
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	// Return the router instance
+	return router
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hyprhex/further
 
 go 1.22.4
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
