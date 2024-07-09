dbfbf75935eb87bd57575c8327ace49216c12a58
hyprhex <swapapis@gmail.com>
2024-07-09 19:19:35 +0100

    Setup handlers for create and show details of one movie by using ID

---
+++ cmd/api/movies.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Return a slice (key and value) of url params
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// Get and convert the ID
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Print out ID
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
