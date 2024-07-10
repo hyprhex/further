36c4b68d3620af92b06af70b85deefdd9b2f1f28
hyprhex <swapapis@gmail.com>
2024-07-10 18:52:43 +0100

    Encode Go object into JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// Maps to hold information
+	data := map[string]string{
+		"status":      "available",
+		"enivrnoment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and cloud not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// For terminal view add a new line
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Write([]byte(js))
+	w.Write(js)
 }
