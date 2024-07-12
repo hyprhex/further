2c03ea89ebe6667dc586c3e6826f12051e2aaed4
hyprhex <swapapis@gmail.com>
2024-07-12 21:35:20 +0100

    Cusome httprouter error to return it as JSON

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() *httprouter.Router {
 	// Initialize a httprouter router
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register URL pattern and handlers
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
